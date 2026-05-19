c8e6387be5af3db942d8c8c0fc58262fe4cb5a3f
SaraMonique13 <moniquesarah5@gmail.com>
2026-05-19 15:01:43 -0300

    Add build assets and image/js minification
    Add compiled build assets (images and minified JS) and update styles. Replace plain uglify import with gulp-uglify and add gulp-imagemin in gulpfile; implement comprimeJavaScript and comprimeImagem tasks and watch entries to compress scripts and images. Update built CSS background color and include generated build scripts (sum.js, aritmetica.js). package.json/package-lock updated to include gulp-uglify and gulp-imagemin (and their dependencies).

--- exercicio_gulp/gulpfile.js
+++ exercicio_gulp/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass =require ('gulp-sass')(require('sass')); //compilador sass
-const uglify = require('uglify');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
 function compilaSass(){
     return gulp.src('./source/style/main.scss')
@@ -10,7 +11,21 @@ function compilaSass(){
     .pipe(gulp.dest('./build/style'));
 }
 
-exports.sass = compilaSass; 
+function comprimeJavaScript(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'));
+}
+
+function comprimeImagem(){
+    return gulp.src('./source/images/*.jpg')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'))
+}
+
+
 exports.default = function(){
     gulp.watch('./source/style/*.scss', {ignoreInitial:false}, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', {ignoreInitial:false}, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*.jpg', {ignoreInitial:false}, gulp.series(comprimeImagem));
 }
