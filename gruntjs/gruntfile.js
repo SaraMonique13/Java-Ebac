5e2a77a920c23dee0d256bad2cda8cfb98007553
SaraMonique13 <moniquesarah5@gmail.com>
2026-06-01 12:56:40 -0300

    Add Grunt setup with Less and Sass tasks
    Create a gruntjs folder with a Gruntfile that configures less (development/production) and sass tasks, plus a custom async task `olaGrunt` that logs "Olá Grunt". Include sample source files (main.less, main.scss), generated outputs (main.css, main2.css, main2.css.map), package.json and package-lock.json with grunt and plugin dependencies, and a .gitignore to exclude node_modules. Default task runs less and sass.

---
+++ gruntjs/gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg:grunt.file.readJSON("package.json"),
+        less: {
+            development: {
+                files: {
+                    "main.css": "main.less"
+                }
+            },
+                production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    "main.css": "main.less"
+                }
+        }
+    },
+     sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('olaGrunt' , function() {
+        const done = this.async();
+        setTimeout(function(){
+            console.log("Olá Grunt");
+            done();
+
+        },3000)
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask("default", ['less', 'sass']);
+}
